71f6b56ac8243efbb9de06828052c6d97854d745
Nishtha Agrawal <nishthaagrawal2008@gmail.com>
2026-08-24 14:34:19 +0530

    Time: 21 ms (35.07%), Space: 111 MB (6.4%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] arr) {
+       HashMap<Integer,Integer> map=new HashMap<>();
+            for(int i=0;i<arr.length;i++)
+            {
+                if(map.containsKey(arr[i]))
+                {
+                    return true;
+                }
+                else
+                {
+                    map.put(arr[i],1);
+                }
+            
+            }
+            return false;
+    }
+}
