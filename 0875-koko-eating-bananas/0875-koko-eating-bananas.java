33840a25447e1793ef1753ac01a86a839e4110e8
Nishtha Agrawal <nishthaagrawal2008@gmail.com>
2026-09-08 15:17:52 +0530

    Time: 20 ms (31.3%), Space: 48.1 MB (30.65%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPossible(int piles[],int hours,int k)
+    {
+        long totalhour=0;
+        for(int i=0;i<piles.length;i++)
+        {
+            totalhour+=(int)Math.ceil((double)piles[i]/k);
+        }
+        return totalhour<=hours;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int l=1;
+        int r=1000000000;
+        int ans=-1;
+        while(l<=r)
+        {
+            int m=l+(r-l)/2;
+            if(isPossible(piles,h,m))
+            {
+               ans=m;
+               r=m-1;
+            }
+            else{
+                l=m+1;
+            }
+        }
+        return ans;
+    }
+}
